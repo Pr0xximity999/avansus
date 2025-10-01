89457da3f4926cc42d732a1638535f8c716cd09d
zlaticaa <cstudyspace@gmail.com>
2025-10-01 11:27:25 +0200

    started game

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+
 int main () {
+	std::cout << "Welcome to the school! Your objectives for this mission are:" << std::endl;
+	std::cout << "	- Collect Assignments (2)" << std::endl;
+	std::cout << "	- Get the Testfiles (1)" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "We suspect the Testfiles to be located in the Server Room. The server room is locked; you'll need an access pass." << std::endl;
+	std::cout << "Good luck!" << std::endl;
 
 }
