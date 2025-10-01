dde1c9a8356aa75ec79e72f2a70cf0bbdb1d0bf9
zlaticaa <cstudyspace@gmail.com>
2025-10-01 11:06:45 +0200

    added room functions

--- src/room/room.hpp
+++ src/room/room.hpp
@@ -1,7 +1,22 @@
 #ifndef ROOM_HPP
 #define ROOM_HPP
+#include "../item/item.h"
+#include <string>
 
 class Room {
+private:
+	std::string Name;
+	std::string Description;
+	std::vector<std::string> Exits;
+	std::vector<Item> Items = *(new std::vector<Item>());
+	std::vector<Room> connectedRooms = *(new std::vector<Room>());
+public:
+	std::string getName();
+	std::string getDescription();
+	std::vector<std::string> getExits();
+	std::vector<Item> getItems();
+	void showMap();
+	std::vector<Room&> getConnectedRooms();
 };
 
 #endif // ROOM_HPP
