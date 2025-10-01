23a0f58586c0740d8890969565d7592ff4af54d9
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-10-01 10:47:38 +0200

    Item class fully implemented

--- src/item/item.h
+++ src/item/item.h
@@ -1,7 +1,6 @@
 //
 // Created by tomsom999 on 10/1/25.
 //
-
 #ifndef WORKSHOP_ITEM_H
 #define WORKSHOP_ITEM_H
 #include <functional>
@@ -9,15 +8,43 @@
 
 
 class Item {
-private:
-    std::string _description;
     std::string _name;
+    std::string _description;
+    int _charges;
     std::function<void()> _useFunc;
-public:
-    Item(std::string name, std::string description, std::function<void()> useFunc);
 
-    void Use() const;
-    void Destroy();
+public:
+    // Default constructor
+    Item(const std::string &name, const std::string &description, int charges, const std::function<void()> &useFunc);
+
+    // Copy constructor
+    Item(const Item& other) = default;
+
+    // Copy assignment
+    Item& operator=(const Item& other) = default;
+
+    // Move constructor
+    Item(Item&& other) noexcept :
+    _name(std::move(other._name)),
+    _description(std::move(other._description)),
+    _charges(std::move(other._charges)),
+    _useFunc(std::move(other._useFunc)){}
+
+    // Move assignment
+    Item& operator=(Item&& other) noexcept {
+        std::swap(_name, other._name);
+        std::swap(_description, other._description);
+        std::swap(_charges, other._charges);
+        std::swap(_useFunc, other._useFunc);
+        return *this;
+    }
+
+    std::string GetName();
+    std::string GetDescription() const;
+    int GetCharges() const;
+
+    void Use();
+    void Destroy() const;
 };
 
 
