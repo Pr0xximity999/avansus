64827d78739b225f6ede71bbb253012e230fa30a
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-10-01 10:56:39 +0200

    temp commit to switch branches

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <thread>
+#define sleep(x) std::this_thread::sleep_for(std::chrono::milliseconds(x));
 int main () {
-
+    std::cout << "Welcome to";
+    sleep(500);
+    std::cout << ".";
+    sleep(500);
 }
