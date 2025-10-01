30596498650a09a33bfeb5f0d29e082ca926f643
Marcy <nyaccoon@gmail.com>
2025-10-01 10:04:05 +0200

    temp update

--- src/player/player.hpp
+++ src/player/player.hpp
@@ -1,8 +1,16 @@
-#ifndef PLAYER_HPP
-#define PLAYER_HPP
-
+#pragma once
+#include "item.h"
+#include <string>
+#include <vector>
 class Player {
-
+private: 
+	/// <summary>
+	/// the name of the player
+	/// </summary>
+	std::string name = "";
+	/// <summary>
+	/// the position of the player refers to an index on the room list
+	/// </summary>
+	int position = 0; 
+	std::vector<item> inventory = new();
 };
-
-#endif // PLAYER_HPP
